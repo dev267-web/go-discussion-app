5777ac31c42e04f23e202dd89b43f307f7e49197
adityaupadhyay0 <dradityaupadhyay.me@gmail.com>
2025-06-07 02:43:01 +0530

    Update comment.go

--- models/comment.go
+++ models/comment.go
@@ -1,1 +1,13 @@
 // comment.go 
+package models
+
+import "time"
+
+// Comment represents a user’s comment on a discussion.
+type Comment struct {
+    ID           int       `json:"id" db:"id"`
+    DiscussionID int       `json:"discussion_id" db:"discussion_id"`
+    UserID       int       `json:"user_id" db:"user_id"`
+    Content      string    `json:"content" db:"content"`
+    CreatedAt    time.Time `json:"created_at" db:"created_at"`
+}
